cec04095d7e883749fa49133c73a967903d0b167
Javier Pastor <jpastor@cerebelum.net>
2019-11-12 01:37:15 +0100

    Add support to automatically export the data to the mysql server.

--- README.md
+++ README.md
@@ -1,46 +1,17 @@
 # Migration procedure:
 
-## We obtain sqlite data and adapt it to mysql.
 
-1. Create or modify database.json configured with sqlite databases:
-```
-{
-    "OmbiDatabase": {
-        "Type":"sqlite",
-        "ConnectionString":"Data Source=/etc/Ombi/Ombi.db"
-    },
-    "SettingsDatabase": {
-        "Type":"sqlite",
-        "ConnectionString":"Data Source=/etc/Ombi/OmbiSettings.db"
-    },
-    "ExternalDatabase": {
-        "Type":"sqlite",
-        "ConnectionString":"Data Source=/etc/Ombi/OmbiExternal.db"
-    }
-}
-```
-2. Download script and extract data from sqlite:
-
-```
-# cd /etc/Ombi
-# wget https://raw.githubusercontent.com/vsc55/ombi_sqlite_mysql/master/ombi_sqlite2mysql.py
-# chmod +x ombi_sqlite2mysql.py
-
-# python2 ombi_sqlite2mysql.py -c /etc/Ombi
-```
-We will create a data_ombi.mysql file with all the inserts that we will add later to the database.
-
-
-## Create DataBase and User:
+## Create database and user in the server MySql/MariaDB:
 ```
 CREATE DATABASE IF NOT EXISTS `Ombi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 CREATE USER 'ombi'@'%' IDENTIFIED BY 'ombi';
 GRANT ALL PRIVILEGES ON `Ombi`.* TO 'ombi'@'%' WITH GRANT OPTION;
 ```
 
+
 ## Create and prepare tables:
 1. Stop ombi
-2. Modify database.json to use mysql.
+2. Create or Modify database.json to use mysql.
 ```
 {
   "OmbiDatabase": {
@@ -58,30 +29,61 @@ GRANT ALL PRIVILEGES ON `Ombi`.* TO 'ombi'@'%' WITH GRANT OPTION;
 }
 ```
 3. Start ombi and wait for it to create the tables.
-4. When you finish creating the tables stop ombi.
-
-  **NOTE:
-  There are times when OmbiExternal tables are not created at startup.
-  You have to access the web for Ombi to detect that they are missing and create them.
-  You can know if you have created them by seeing if there are tables such as PlexEpisode, PlexSeasonsContent or SonarrCache.**
-  
-5. Empty all tables with data except:
-- __EFMigrationsHistory
-
-## Import data.
-
-Now we import the data into our mysql database with the content of the file we generated before "data_ombi.mysql".
-
-**NOTE: When importing the data, the order of the following keys must be taken into account.**
-
-**Plex:**
-1. PlexServerContent
-2. PlexSeasonsContent
-3. PlexEpisode
-
-**Emby:**
-1. EmbyContent
-2. EmbyEpisode
+4. We access the ombi website to finish generating the missing tables. ExternalDatabase tables are not created until they are first accessed.
+   **No need to start the wizard, just access the web.**
+5. Stop ombi.
 
 
-And that seems to me to be everything.
+## Data Migration
+1. In the directory where the Ombi databases are, we will create the file **"migration.json"**, in this file we will configure the databases that we want to export.
+```
+{
+    "OmbiDatabase": {
+        "Type":"sqlite",
+        "ConnectionString":"Data Source=/etc/Ombi/Ombi.db"
+    },
+    "SettingsDatabase": {
+        "Type":"sqlite",
+        "ConnectionString":"Data Source=/etc/Ombi/OmbiSettings.db"
+    },
+    "ExternalDatabase": {
+        "Type":"sqlite",
+        "ConnectionString":"Data Source=/etc/Ombi/OmbiExternal.db"
+    }
+}
+```
+If we do not want to export OmbiExternal.
+```
+{
+    "OmbiDatabase": {
+        "Type":"sqlite",
+        "ConnectionString":"Data Source=/etc/Ombi/Ombi.db"
+    },
+    "SettingsDatabase": {
+        "Type":"sqlite",
+        "ConnectionString":"Data Source=/etc/Ombi/OmbiSettings.db"
+    }
+}
+```
+
+2. Download the script and install the dependencies.
+```
+$ cd /etc/Ombi
+$ wget https://raw.githubusercontent.com/vsc55/ombi_sqlite_mysql/master/ombi_sqlite2mysql.py
+$ chmod +x ombi_sqlite2mysql.py
+
+$ apt-get install python-mysqldb 	# Debian/Ubuntu
+$ emerge -va mysqlclient			# Gentoo
+```
+
+3. Start data migration.
+The script will empty the tables from the MySQL/MariaDB database and automatically migrate the data from SQLite to MySQL/MariaDB.
+```
+$ python2 ombi_sqlite2mysql.py -c /etc/Ombi --host mysql.local --port 3306 --db Ombi --user ombi --passwd ombi
+```
+**NOTE: It is recommended to use --force to empty all tables before migrating data.**
+```
+$ python2 ombi_sqlite2mysql.py -c /etc/Ombi --host mysql.local --port 3306 --db Ombi --user ombi --passwd ombi --force
+```
+
+4. Start ombi and test if everything works fine.
--- ombi_sqlite2mysql.py
+++ ombi_sqlite2mysql.py
@@ -1,61 +1,90 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Migration tool by SQLite to MySql/MariaDB for ombi
+#
+# Copyright © 2019  Javier Pastor (aka VSC55)
+# <jpastor at cerebelum dot net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
+import time
 import codecs
 import json
-import subprocess
 import sqlite3
+import MySQLdb
 from optparse import OptionParser
 
-mysql_db_file = "data_ombi.mysql"
+json_file_migration = "migration.json"
+json_file_database = "database.json"
 json_db_file = ""
-json_db_data = []
+json_db_data = None
 list_db = ['OmbiDatabase', 'SettingsDatabase', 'ExternalDatabase']
-list_db_process = []
+list_db_process = None
+
+check_count_data = {}
+
+mysql_db_file = "data_ombi.mysql"
+mysql_log_err = "insert_error.log"
+mysql_cfg = None
+mysql_conn = None
+mysql_list_tables_no_clean = ['__EFMigrationsHistory']
+mysql_list_error = []
 
 
 
-# TODO: _process y _backticks codigo pendiente de eliminar 
-def _backticks(line, in_string):
-    """Replace double quotes by backticks outside (multiline) strings
-
-    >>> _backticks('''INSERT INTO "table" VALUES ('"string"');''', False)
-    ('INSERT INTO `table` VALUES (\\'"string"\\');', False)
-
-    >>> _backticks('''INSERT INTO "table" VALUES ('"Heading''', False)
-    ('INSERT INTO `table` VALUES (\\'"Heading', True)
-
-    >>> _backticks('''* "text":http://link.com''', True)
-    ('* "text":http://link.com', True)
-
-    >>> _backticks(" ');", True)
-    (" ');", False)
-
-    """
-    new = ''
-    for c in line:
-        if not in_string:
-            if c == "'":
-                in_string = True
-            elif c == '"':
-                new = new + '`'
-                continue
-
-        elif c == "'":
-            in_string = False
-        new = new + c
-    return new, in_string
-
-def _process(opts, lines):
-    in_string = False
-    for line in lines:
-        if not in_string:
-            if line is None:
-                continue
-        line, in_string = _backticks(line, in_string)
-        yield line
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
 
+# https://stackoverflow.com/questions/3160699/python-progress-bar
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()        
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
+
+
+
+def _save_file(file_name, data):
+    sys.stdout.write("- Keeping in ({0})... ".format(file_name))
+    try:
+        with open(file_name, 'w') as f:
+            for line in data:
+                f.write('%s\n' % line)
+        
+    except IOError as ex:
+        print(" ERROR!")
+        print("I/O error({0}): {1}".format(ex.errno, ex.strerror))
+        return False
+    except:
+        print(" ERROR!")
+        print("Unexpected error:", sys.exc_info()[0])
+        return False
+    else:
+        print("OK!")
+        return True
 
 def _read_json(file_json, def_return=None):
     return_date = def_return
@@ -89,7 +118,7 @@ def _check_read_config(opts):
         print ("The config path does not exist or is not a directory !!")
         sys.exit()
     
-    json_db = os.path.join(opts.config, "database.json")
+    json_db = os.path.join(opts.config, json_file_migration)
     if not os.path.isfile(json_db):
         print("File {0} not exist!!!".format(json_db))
         sys.exit()
@@ -100,7 +129,7 @@ def _check_read_config(opts):
         sys.exit()
     
     list_db_process = []
-    print("Check database.json:")
+    print("Check {0}:".format(json_file_migration))
     for db_name in list_db:
         if json_db_data[db_name]['Type'].lower() == "sqlite":
             list_db_process.append(db_name)
@@ -114,6 +143,254 @@ def _check_read_config(opts):
         print ("It is not necessary to update all databases are migrated.")
         sys.exit()
 
+def _check_config_mysql(opts):
+    #TODO: pendiente leer config de database.json
+    global mysql_cfg
+    mysql_cfg = None
+    if opts.host:
+        mysql_cfg = {
+            'host': opts.host,
+            'port': opts.port,
+            'db': opts.db,
+            'user': opts.user,
+            'passwd': opts.passwd,
+            'connect_timeout': 2,
+            'use_unicode': True,
+            'charset': 'utf8'
+        }
+
+def _mysql_connect():
+    global mysql_conn
+
+    if mysql_cfg:
+        _mysql_disconnect()
+        print("Connecting mysql...")
+        try:
+            mysql_conn = MySQLdb.connect(**mysql_cfg)
+            print("Connection OK!")
+
+        except MySQLdb.Error, e:
+            try:
+                print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                sys.exit()
+            except IndexError:
+                print "MySQL Error: %s" % str(e)
+                sys.exit()
+        except TypeError, e:
+            print(e)
+            sys.exit()
+        except ValueError, e:
+            print(e)
+            sys.exit()
+
+def _mysql_disconnect():
+    global mysql_conn
+
+    if mysql_conn is not None:
+        print("Disconnecting mysql...")
+        mysql_conn.close()
+        mysql_conn = None
+        print("Disconnect OK!")
+
+def _mysql_migration(data_dump):
+    global mysql_conn
+    global mysql_list_error
+
+    if mysql_conn is not None:
+        print ("Start Migration:")
+        
+        cur = mysql_conn.cursor()
+        cur.execute("SET FOREIGN_KEY_CHECKS = 0;")
+
+        count_commit = 0
+
+        str_insert = "INSERT INTO"
+        for i in progressbar(data_dump, "- Progress: ", 60):
+            if i is None:
+                #print("Ignorer 1:", i)
+                continue
+            elif len(i) < len(str_insert):
+                #print("Ignorer 2:", i)
+                continue
+            elif i[:len(str_insert)].upper() != str_insert:
+                #print("Ignorer 3:", i)
+                continue
+            else:
+                try:
+                    cur.execute(i)
+                    if count_commit == 500:
+                        mysql_conn.commit()
+                        count_commit = 0
+                    else:
+                        count_commit += 1
+                    
+                except MySQLdb.Error, e:
+                    mysql_conn.rollback()
+                    show_msg_err = True
+                    try:
+                        str_msg = "* MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                        if e.args[0] == 1452:
+                            # Cannot add or update a child row: a foreign key constraint fails
+                            show_msg_err = False
+                        elif e.args[0] == 1062:
+                            #  Duplicate entry
+                            show_msg_err = False
+
+                    except IndexError:
+                        str_msg = "* MySQL Error: %s" % str(e)
+                    
+                    mysql_list_error.append(str_msg)
+                    mysql_list_error.append(i)
+                    if show_msg_err:
+                        print str_msg
+                        print "* Query: {0}".format(i)
+                    
+                    #sys.exit()
+                    #time.sleep(1)
+                        
+                except TypeError, e:
+                    mysql_conn.rollback()
+                    print(e)
+                    print "Query: {0}".format(i)
+                    sys.exit()
+
+                except ValueError, e:
+                    mysql_conn.rollback()
+                    print(e)
+                    print "Query: {0}".format(i)
+                    sys.exit()
+
+        if count_commit > 0:
+            mysql_conn.commit()
+        
+        cur.execute("SET FOREIGN_KEY_CHECKS = 1;")
+        print ("Migration completed.")
+
+        mysql_conn.commit()
+
+        cur.close()
+        cur = None
+
+def _mysql_migration_check():
+    global mysql_conn
+    global check_count_data
+    print("Check Migartion...")
+    
+    if mysql_conn is None:
+        isOkMigration = False
+    else:
+        isOkMigration = True
+
+        cur = mysql_conn.cursor()
+
+        q = "SET group_concat_max_len = 1024 * 1024 * 100;"
+        q += "SELECT CONCAT('SELECT * FROM (',GROUP_CONCAT(CONCAT('SELECT ',QUOTE(tb),' Tables_in_database, COUNT(1) \"Number of Rows\" FROM ',db,'.',tb) SEPARATOR ' UNION '),') A;') INTO @sql FROM (SELECT table_schema db,table_name tb FROM information_schema.tables WHERE table_schema = DATABASE()) A;"
+        q += "PREPARE s FROM @sql;"
+        cur.execute(q)
+
+        # Si se ejecuta todo en el mismo execute no retorna datos!
+        q = "EXECUTE s; DEALLOCATE PREPARE s;"
+        cur.execute(q)
+        list_tables = cur.fetchall()
+
+        for table, count in list_tables:
+            if table in check_count_data and count != check_count_data[table]:
+                isOkMigration = False
+                print("- [ERR] -> {0} -> [SQLite ({1}) / MySQL ({2})] = {3}".format(table, check_count_data[table], count, check_count_data[table] - count))
+            else:
+                print("- [OK] -> {0}".format(table))
+
+        cur.close()
+        cur = None
+
+        if isOkMigration == True:
+            print ("Check Migartion OK! :)")
+        else:
+            print ("Migartion Failed!!! ;,,(")
+
+def _mysql_tables_clean():
+    global mysql_conn
+    global check_count_data
+
+    print ("Clean tables...")
+    if mysql_conn is not None:
+        cur = mysql_conn.cursor()
+
+        # TODO: Pendiente ver por que si no se vacia __EFMigrationsHistory no se importan todos los datos correctamente!!!!!!
+
+        #Retorna datos no fiables, en ocasiones dice que hay 0 registros y si tiene registros.
+        #q = "SELECT table_name, table_rows FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{0}';".format(mysql_cfg['db'])
+
+        q = "SET group_concat_max_len = 1024 * 1024 * 100;"
+        q += "SELECT CONCAT('SELECT * FROM (',GROUP_CONCAT(CONCAT('SELECT ',QUOTE(tb),' `Table`, COUNT(1) `Rows` FROM ',db,'.',tb) SEPARATOR ' UNION '),') A "
+        q += "ORDER BY "
+        #q += "`Table` = \"__EFMigrationsHistory\" DESC, "
+        #q += "`Table` = \"AspNetUsers\" DESC, `Table` = \"ChildRequests\" DESC, `Table` = \"MovieRequests\" DESC, ";
+        #q += "`Table` = \"Issues\" DESC, `Table` = \"IssueComments\" DESC, `Table` = \"IssueCategory\" DESC, ";
+        #q += "`Table` = \"EmbyContent\" DESC, `Table` =  \"EmbyEpisode\" DESC, ";
+        #q += "`Table` = \"PlexServerContent\" DESC, `Table` = \"PlexSeasonsContent\" DESC, `Table` = \"PlexEpisode\" DESC, ";
+        q += "`Table` ASC ";
+        q += ";')"
+        q += "INTO @sql FROM (SELECT table_schema db,table_name tb FROM information_schema.tables WHERE table_schema = DATABASE()) A;"
+        q += "PREPARE s FROM @sql;"
+        cur.execute(q)
+
+        # Si se ejecuta todo en el mismo execute no retorna datos!
+        q = "EXECUTE s; DEALLOCATE PREPARE s;"
+        cur.execute(q)
+        list_tables = cur.fetchall()
+
+        # Desactivamos la comprobacion de tablas relacionadas.
+        cur.execute("SET FOREIGN_KEY_CHECKS = 0;")
+
+        for table, count in list_tables:
+            if table in mysql_list_tables_no_clean:
+                print("- [NOT CLEAN] -> {0}".format(table))
+                check_count_data[table] = count
+                continue
+            else:
+                if count == 0:
+                    print("- [EMPTY] -> {0}".format(table))
+                    continue
+                else:
+                    print("- [CLEANING] -> {0} -> rows: {1}".format(table, count))
+                    try:
+                        q = "TRUNCATE TABLE `{0}`;".format(table)
+                        cur.execute(q)
+                        mysql_conn.commit()
+                        
+                    except MySQLdb.Error, e:
+                        #mysql_conn.rollback()
+                        show_msg_err = True
+                        try:
+                            str_msg = "* MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                        except IndexError:
+                            str_msg = "* MySQL Error: %s" % str(e)
+                        
+                        mysql_list_error.append(str_msg)
+                        mysql_list_error.append(q)
+                        if show_msg_err:
+                            print str_msg
+                            print "* Query: {0}".format(q)
+                
+
+        # Volvemos a activar la comprobacion de tablas relacionadas.
+        cur.execute("SET FOREIGN_KEY_CHECKS = 1;")
+
+        mysql_conn.commit()
+        cur.close()
+        cur = None
+
+def _mysql_database_json_update(opts):
+    json_data = {}
+    for db_name in list_db:
+        json_data[db_name] = {
+            "Type": "MySQL",
+            "ConnectionString": "Server={0};Port={1};Database={2};User={3};Password={4}".format(mysql_cfg['host'], mysql_cfg['port'], mysql_cfg['db'], mysql_cfg['user'], mysql_cfg['passwd'])
+        }
+    json_mysql = os.path.join(opts.config, json_file_database)
+    _save_json(json_mysql, json_data)
+
 def _sqlite_dump(opts):
     print("Dump SQLite:")
     for db_name in list_db_process:
@@ -124,7 +401,7 @@ def _sqlite_dump(opts):
             continue
 
         yield('--')
-        yield('--DataBase: %s;' % db_name)
+        yield('-- DataBase: %s;' % db_name)
         yield('--')
 
         sqlite_db_file =  connection_str.split("=")[1]
@@ -134,6 +411,8 @@ def _sqlite_dump(opts):
         print ("OK!")
 
 def _iterdump(connection, db_name):
+    global check_count_data
+
     cu = connection.cursor()
 
     q = "SELECT name, type FROM sqlite_master WHERE sql NOT NULL AND type == 'table' ORDER BY "
@@ -150,14 +429,14 @@ def _iterdump(connection, db_name):
 
     schema_res = cu.execute(q)
     for table_name, type in schema_res.fetchall():
+        if table_name not in check_count_data:
+            check_count_data[table_name] = 0
+
         if table_name in ['sqlite_sequence', 'sqlite_stat1'] or table_name.startswith('sqlite_'):
             continue
         elif cu.execute("SELECT COUNT(*) FROM {0}".format(table_name)).fetchone()[0] < 1:
             continue
-        else:
-            #Ignorer CREATE TABLE
-            pass
-            
+                    
 
         # TODO: Pendiente agrupar insert para una exportacion mas rapida.
 
@@ -172,7 +451,7 @@ def _iterdump(connection, db_name):
             q_col += '`{0}`'.format(col_n)
 
         yield('--')
-        yield('--Table: %s;' % table_name)
+        yield('-- Table: %s;' % table_name)
         yield('--')
 
         q = "SELECT '"
@@ -191,52 +470,76 @@ def _iterdump(connection, db_name):
             #q_insert = q_insert.replace(",'f'", ",'0'")
 
             q_insert = 'INSERT INTO `{0}` ({1}) VALUES({2})'.format(table_name, q_col, q_insert)
+            check_count_data[table_name] += 1
             yield("%s;" % q_insert)
-
 
 def _save_dump(opts, data):
     print ("Save dump:")
     dump_db_file =  os.path.join(opts.config, mysql_db_file)
-    sys.stdout.write("- Keeping in ({0})... ".format(dump_db_file))
-    try:
-        with open(dump_db_file, 'w') as f:
-            for line in data:
-                f.write('%s\n' % line)
-        
-    except IOError as ex:
-        print(" ERROR!")
-        print("I/O error({0}): {1}".format(ex.errno, ex.strerror))
-    except:
-        print(" ERROR!")
-        print("Unexpected error:", sys.exc_info()[0])
-    else:
-        print("OK!")
+    _save_file(dump_db_file, data)
 
+def _save_error_log(opts, data):
+    if data is not None and len(data) > 0:
+        print ("Save Log Error Mysql Insert:")
+        log_file =  os.path.join(opts.config, mysql_log_err)
+        _save_file(log_file, data)
+    
 
-def dump(obj):
-  for attr in dir(obj):
-    print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
 
 def main():
+    global check_count_data
+    global mysql_list_tables_no_clean
+
     op = OptionParser()
-    op.add_option('-c', '--config')
-    op.add_option('-d', '--database')
-    op.add_option('-u', '--username')
-    op.add_option('-p', '--password')
+    op.add_option('-c', '--config', default="/etc/Ombi", help="path folder config ombi, default /etc/Ombi")
+    op.add_option('', '--host', help="host server mysql/mariadb, If empty, file is generated with inserts.")
+    op.add_option('', '--port', type="int", default=3306, help="port server mysql/mariadb, default 3306")
+    op.add_option('', '--db', default="Ombi", help="name database, default Ombi")
+    op.add_option('', '--user', default="ombi", help="user name mysql/mariadb, default ombi")
+    op.add_option('', '--passwd', default="", help="passwd mysql/mariadb, defalt empty")
+    op.add_option('-f', '--force', action="store_true",  default=False, help="force to clean all tables")
     opts, args = op.parse_args()
 
+    if opts.force:
+        mysql_list_tables_no_clean = []
+
+
     _check_read_config(opts)
-    
+    _check_config_mysql(opts)
+
+
     data_dump = []
     for line in _sqlite_dump(opts):
         data_dump.append(line)
 
+    # Si no se aNaden da error al arrancar Ombi ya que intenta crear las tablas pero ya existen.
+    data_dump.append("INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`) VALUES ('20191103213915_Inital', '2.2.6-servicing-10079');")
+    data_dump.append("INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`) VALUES ('20191103205915_Inital', '2.2.6-servicing-10079');")
+    data_dump.append("INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`) VALUES ('20191102235852_Inital', '2.2.6-servicing-10079');")
+    check_count_data['__EFMigrationsHistory'] += 3
+
     if 1 == 1:
         _save_dump(opts, data_dump)
     else:
-        for line in data_dump:
-            print (line)
+        print (line)
+
+    if mysql_cfg is not None:
+        _mysql_connect()
+        _mysql_tables_clean()
+
+        
+        #time.sleep(5)
+
+        _mysql_migration(data_dump)
+        _mysql_migration_check()
+        _mysql_disconnect()
+
+        _mysql_database_json_update(opts)
+        _save_error_log(opts, mysql_list_error)        
+
 
 if __name__ == "__main__":
+    print("Migration tool by SQLite to MySql/MariaDB for ombi (3.0) By VSC55")
+    print("")
     main()
